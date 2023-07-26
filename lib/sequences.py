d4fc8f71b1014eb43fc67dde462a64bbed9fa4fc
Prateek <teeksingh@gmail.com>
2023-07-26 11:36:27 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+   list = []
+   first = 0
+   second = 1
+   temp = 0
+
+   for i in range(length):
+      if i in {0,1}:
+         list.append(i)
+      else:
+         list.append(first +second)
+         temp = second
+         second = first + second
+         first = temp
+
+   print(list)
+      
